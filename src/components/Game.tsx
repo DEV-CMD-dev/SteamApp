0a1abac02cb739161cb1fd67a18f4f2c09161da9
Maksym-Rubel <rubelmaksum2404@gmail.com>
2026-07-20 10:58:58 +0300

    Main_Page_Sceleton

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,16 +1,27 @@
 import "../css/game.css";
 
-export default function Game({ price, discount, img }: { price: number; discount: number; img: string }) {
+export default function Game({ img, price, discount, loading }: { img: string, price: number, discount: number, loading: boolean }) {
     return (
-        <div className="game">
-            <img className="game-image" src={img} alt="Game Image" />
-            <div className="game-price">
-                <div className="discount"><p>-{discount}%</p></div>
-                <div className="price">
-                    <p className="original-price">${price.toFixed(0)}</p>
-                    <p className="discounted-price">${(price * (1 - discount / 100)).toFixed(0)}</p>
+
+        <>
+            {!loading ? (
+                <div className="game-card">
+                    <img className="game-image" src={img} alt="Game Cover" />
+                    <div className="game-price">
+                        <div className="discount">
+                            <p>-{discount}%</p>
+                        </div>
+                        <div className="price">
+                            <p className="original-price">${price.toFixed(0)}</p>
+                            <p className="discounted-price">${(price * (1 - discount / 100)).toFixed(0)}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    )
+            ) : (
+                <div className="game-card-null"/>
+            )}
+
+        </>
+
+    );
 }
--- src/pages/StorePage.tsx
+++ src/pages/StorePage.tsx
@@ -6,51 +6,75 @@ import { useState, useEffect } from "react";
 export default function StorePage() {
     const [games, setGames] = useState([]);
     const [tags, setTags] = useState([]);
+    const [gamePageNumber, setgamePageNumber] = useState(1);
+    const [tagPageNumber, settagPageNumber] = useState(1);
+    const [gameLoading, setgameLoading] = useState(true);
 
+    const fetchGames = async () => {
+        setgameLoading(true);
+        try {
+            const response = await fetch(`http://localhost:5215/api/Game?pageNumber=${gamePageNumber}&pageSize=3`);
+            const data = await response.json();
+            setGames(data);
+        } catch (error) {
+            console.error("Error fetching games:", error);
+        }
+        finally {
+            setgameLoading(false);
+        }
+    };
+    const fetchTags = async () => {
+        try {
+            const response = await fetch(`http://localhost:5215/api/Tag?pageNumber=${tagPageNumber}&pageSize=5`);
+            const data = await response.json();
+            setTags(data);
+        } catch (error) {
+            console.error("Error fetching tags:", error);
+        }
+    };
 
     useEffect(() => {
-        const fetchGames = async () => {
-            try {
-                const response = await fetch("http://localhost:5215/api/Game");
-                const data = await response.json();
-                setGames(data);
-            } catch (error) {
-                console.error("Error fetching games:", error);
-            }
-        };
-        const fetchTags = async () => {
-            try {
-                const response = await fetch("http://localhost:5215/api/Tag");
-                const data = await response.json();
-                setTags(data);
-            } catch (error) {
-                console.error("Error fetching tags:", error);
-            }
-        };
-
         fetchGames();
+    }, [gamePageNumber]);
+    useEffect(() => {
         fetchTags();
-    }, []);
+    }, [tagPageNumber]);
 
     return (
         <>
             <div className="store-header" />
             <div className="store-background">
-                <div className="games-container">
-                    {games.slice(0, 3).map((game: any) => (
-                        <Game key={game.id} price={game.price} discount={20} img={game.coverImage} />
-                    ))}
+
+                <div className="btn-container">
+                    <button className="prev-btn" onClick={() => setgamePageNumber(num => num > 1 ? num - 1 : num)}></button>
+                    <div className="games-container">
+                        {gameLoading ? (
+                            [...Array(3)].map((index) => (
+                                <Game key={index} img="" price={0} discount={0} loading={true} />
+                            ))
+                        ) : (
+                            games.slice(0, 3).map((game: any) => (
+                                <Game key={game.id} img={game.coverImage} price={10} discount={10} loading={false} />
+                            ))
+                        )}
+                    </div>
+                    <button className="next-btn" onClick={() => setgamePageNumber(num => num <= 3 ? num + 1 : num)}></button>
                 </div>
-                
+
                 <div className="categories-section">
                     <h3>Browse by Category</h3>
-                    <div className="category-container">
+                    <div className="btn-container">
+                        <button className="prev-btn" onClick={() => settagPageNumber(num => num > 1 ? num - 1 : num)}></button>
+                        <div className="category-container">
 
-                        {tags.slice(0,5).map((tag: any) => (
-                            <Category key={tag.id} name={tag.name} imgUrl={tag.picture} />
-                        ))}
+                            {tags.slice(0, 5).map((tag: any) => (
+                                <Category key={tag.id} name={tag.name} imgUrl={tag.picture} />
+                            ))}
+                        </div>
+                        <button className="next-btn" onClick={() => settagPageNumber(num => num <= 3 ? num + 1 : num)}></button>
                     </div>
                 </div>
+
             </div>
         </>
     )
