b3b6ebed0a3924678209897104456261384d0203
Maksym-Rubel <rubelmaksum2404@gmail.com>
2026-07-20 10:58:58 +0300

    Main_Page_Sceleton

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -2,8 +2,15 @@ import "../css/category.css";
 
 export default function Category({ name, imgUrl }: { name: string; imgUrl: string }) {
     return (
-        <div className="category" style={{ backgroundImage: `url(${imgUrl})` }}>
-            <p className="category-text">{name}</p>
+        <div className="category">
+            <div
+                className="category-bg"
+                style={{ backgroundImage: `url(${imgUrl})` }}
+            />
+
+            <div className="category-content">
+                <p>{name.toUpperCase()}</p>
+            </div>
         </div>
     )
 }
