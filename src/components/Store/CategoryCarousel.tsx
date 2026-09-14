c105d99b067dc67eb2094966388701c0fc8a569b
RomanVilchynskyi <romaxa207207@gmail.com>
2026-09-14 12:32:05 +0300

    add link for categories in main page

--- src/components/Store/CategoryCarousel.tsx
+++ src/components/Store/CategoryCarousel.tsx
@@ -4,6 +4,7 @@ import { tagService } from "../../services/tagService";
 import Tag from "./Tag";
 import styles from "../../css/Store/CategoryCarousel.module.css";
 import arrow from "../../assets/store/arrow.svg";
+import { Link } from "react-router-dom";
 
 export default function CategoryCarousel() {
     const [items, setItems] = useState<TagDto[]>([]);
@@ -60,11 +61,12 @@ export default function CategoryCarousel() {
                 <div className={styles.carouselWrapper}>
                     <div className={styles.carouselRow}>
                         {items.map((tag) => (
-                            <div
+                            <Link
+                                to={`/category/${tag.id}`}
                                 className={styles.cardWrapper}
                                 key={tag.id}>
                                 <Tag {...tag} />
-                            </div>
+                            </Link>
                         ))}
                     </div>
                 </div>
